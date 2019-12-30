2c88ef1ced5b57cd31c5c3b33869440b0d9fd600
Levkovits <5380787@gmail.com>
2019-12-30 10:55:39 +0200

    intermidiat, tests failing
    Former-commit-id: 7f97f4495967c9b1ea3c79fc07c60360c1713a43

--- object/Mat4Test.h
+++ object/Mat4Test.h
@@ -15,7 +15,8 @@ BASE_FUNCTION_PTR(Mat4Test, findLoc, int row, int col, int* loc)
 FUNCTION_PTR(Mat4Test, getWidth, int * width)
 END_FUNCTIONS(Mat4Test)
 
-FUNCTION_H(Mat4Test, findLoc, int row, int col,int * loc);
+BASE_FUNCTION_H(Mat4Test, findLoc, int row, int col,int * loc);
+//typedef struct findLoc_t_ findLoc_t;
 FUNCTION_H(Mat4Test, getWidth, int* width);
 
 DEF_INIT_CLASS(Mat4Test)
