d7d81e9a5ea54bbbfdaf80d95fedc56bf7fccd12
Levkovits <5380787@gmail.com>
2020-01-07 14:16:32 +0200

    added cache & scope tests, fixed few errors, all tests are passing.
    Former-commit-id: 94124082c9e1606cd6b0dbcf62e78198e4b2dc8d

--- object/Globals.h
+++ object/Globals.h
@@ -21,7 +21,7 @@ COOP_API extern iCache* TheGlobalCache;
 typedef enum CACHE_TYPES_t { IN_MEMORY_CACHE_ = 0, HEAP_CACHE_ = 1, NUM_CACHE_TYPES } CACHE_TYPES;
 
 COOP_API void CreateGlobalCache(int size,const char *  name, CACHE_TYPES type);
-COOP_API void DestroyGlobalCache(iCache*);
+COOP_API void DestroyGlobalCache();
 
 
 #ifdef __cplusplus
--- object/ObjectTest.h
+++ object/ObjectTest.h
@@ -14,6 +14,14 @@
 extern "C" {
 #endif
 
+	COOP_API int LOCAL_SCOPE_END__WhenDefiningObjectInside_ThenFreesThemAll();
+
+	COOP_API int LOCAL_SCOPE_END__WhenDefiningObjectOutside_thenDoesntFreeThem();
+
+	COOP_API int LOCAL_SCOPE_END__TheLocalScopeEnd_FreesOnlyTheLastInnerScopeThatIsntFreedYet();
+
+	COOP_API int LOCAL_SCOPE_END__WhenMostInnerScopeHasNoObjects_ThenDoesntCrash();
+
 	COOP_API int New_WhenNew_ThenReturnesPointerInCache();
 
 	COOP_API int New_WhenNew_ConstructsBlockWithRightSize();
