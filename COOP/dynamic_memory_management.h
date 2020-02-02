739fe308b95dc1b029104f34caf8044bc7f0e22e
ShmuelFine <shmuel.v.fine@gmail.com>
2020-02-02 10:57:10 +0200

    Adding memory manager

--- COOP/dynamic_memory_management.h
+++ COOP/dynamic_memory_management.h
@@ -2,6 +2,7 @@
 #define __GLOBALS__H_
 
 #include "ExportDefs.h"
+#include "coop.h"
 #include "iCache.h"
 #include <stdbool.h>
 
@@ -12,17 +13,24 @@ extern "C" {
 
 COOP_API extern iCache* TheGlobalCache;
 
-typedef enum CACHE_TYPES_t { IN_MEMORY_CACHE_ = 0, HEAP_CACHE_ = 1, NUM_CACHE_TYPES } CACHE_TYPES;
+typedef enum CACHE_TYPES_t { STACK_BASED_MEMORY, HEAP_BASED_MEMORY, NUM_MEMORY_TYPES } CACHE_TYPES;
 
 COOP_API bool CreateGlobalCache(int size, CACHE_TYPES type);
 COOP_API void DestroyGlobalCache();
+
+DEF_CLASS(MemoryManager);
+END_DEF(MemoryManager);
+
+FUNCTIONS(MemoryManager, int size, CACHE_TYPES type);
+END_FUNCTIONS(MemoryManager);
+
 
 #define NEW(dest,whatToPutThere)\
 	{\
 		FUN(TheGlobalCache, AddNewBlock),(sizeof(whatToPutThere)),(void*)&dest CALL\
 	}
 
-	#define DELETE_OBJ(buff) FUN(TheGlobalCache, RemoveBlock), buff CALL buff = NULL
+#define DELETE_OBJ(buff) FUN(TheGlobalCache, RemoveBlock), buff CALL buff = NULL
 
 #ifdef __cplusplus
 }
