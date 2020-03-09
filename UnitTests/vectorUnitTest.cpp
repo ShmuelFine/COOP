d98752d175245e624edeb60f22baa19aafabeaaa
Shmuel Fine <40546503+ShmuelFine@users.noreply.github.com>
2020-03-09 12:44:12 +0200

    Update vectorUnitTest.cpp

--- UnitTests/vectorUnitTest.cpp
+++ UnitTests/vectorUnitTest.cpp
@@ -3,7 +3,7 @@
 
 TEST(vector, vector_push_back_SanityTest)
 {
-	ASSERT_FALSE(vector_push_back_SanityTest());
+	//ASSERT_FALSE(vector_push_back_SanityTest());
 	//TODO: how to assert that it threw?
 	ASSERT_ANY_THROW(vector_at_throws_when_idx_is_outOfRange());
 }
