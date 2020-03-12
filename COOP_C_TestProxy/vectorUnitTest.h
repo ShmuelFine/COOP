b09ab9cde3572092fe4332345687ea6e350c14eb
Hanna Gurwitz <hannabonert@gmail.com>
2020-03-12 11:34:53 +0200

    intermediate commit of vector
    Former-commit-id: e486e54429d629c2d7b88872152ff7145c0feb0b

--- COOP_C_TestProxy/vectorUnitTest.h
+++ COOP_C_TestProxy/vectorUnitTest.h
@@ -1,15 +1,15 @@
 #ifndef __VECTOR_UNIT_TEST__H__
 #define __VECTOR_UNIT_TEST__H__
 
+#include "ExportDefs.h"
+#include "COOP.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include "ExportDefs.h"
-#include "COOP.h"
-
 	COOP_API FUN_DECL(vector_push_back_SanityTest);
-	COOP_API FUN_DECL(vector_at_throws_when_idx_is_outOfRange);
+	COOP_API FUN_DECL(vector_at_throws_when_idx_is_outOfRange, int* tester);
 
 #endif
 
--- UnitTests/vectorUnitTest.cpp
+++ UnitTests/vectorUnitTest.cpp
@@ -1,9 +1,17 @@
 #include "pch.h"
 #include "vectorUnitTest.h"
-
-TEST(vector, vector_push_back_SanityTest)
+//
+TEST(vector, vector_push_back_sanity)
 {
 	ASSERT_FALSE(vector_push_back_SanityTest());
-	//TODO: how to assert that it threw?
-	ASSERT_ANY_THROW(vector_at_throws_when_idx_is_outOfRange());
+//	//TODO: how to assert that it threw?
+//	//ASSERT_ANY_THROW(vector_at_throws_when_idx_is_outOfRange());
+}
+
+TEST(vector, vector_at_throws_when_idx_is_outOfRange_sanity)
+{
+    int tester;
+    auto ret = vector_at_throws_when_idx_is_outOfRange(&tester);
+    ASSERT_EQ(IN_THROWING_VALUE, ret);
+    ASSERT_EQ(0, tester);
 }
