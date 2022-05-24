7470916f945b5feb00ae8141ce044c10e5ec3101
RonenCode <212377808@mby.co.il>
2022-05-24 13:54:55 +0300

    finish the classTests in our platform and added inheritTests file.
    Former-commit-id: ba90730bb5178deffe0991ea767377e6369099db

--- UnitTestC/ClassTest.c
+++ UnitTestC/ClassTest.c
@@ -10,29 +10,44 @@ int is_the_test_platform_run_as_well()
 	int a = 10;
 	int b = 11;
 	TEST_ASSERT(a != b);
-	//act 
-
 
 	//assert
 	return TEST_PASS;
 
 }
-int  Class_CallSimpleFunction_SanityTest() 
-{	
-		// Arrange
-		CREATE(BaseClassExample, B), 10, 3);
-		// Act
-		int retVal = 0;
-		FUN(&B, GetVolume), & retVal CALL
-			// Assert
-		TEST_ASSERT(retVal == 10 * 3);
-		return TEST_PASS;
-}
 
+FUN_IMPL(Class_CallSimpleFunction) {
+	// Arrange
+	CREATE(BaseClassExample, B), 10, 3);
+	// Act
+	int retVal = 0;
+	FUN(&B, GetVolume), & retVal CALL
+		// Assert
+		TEST_ASSERT(retVal == 10 * 3);
+	return TEST_PASS;
+}END_FUN;
+
+FUN_IMPL(Rectangle_SanityTest)
+{
+	// Arrange
+	CREATE(Rectangle, Rect), 10, 3);
+
+	// Act
+	int retVal = 0;
+	FUN(&Rect, GetArea), & retVal CALL
+
+	// Assert
+		TEST_ASSERT(retVal == 10 * 3);
+	return TEST_PASS;
+
+
+}END_FUN
 
 int main() {
 	INIT_TESTS();
 	RUN_TEST(is_the_test_platform_run_as_well);
-	
+	RUN_TEST(Class_CallSimpleFunction);
+	RUN_TEST(Rectangle_SanityTest);
+
 }
 
---
+++ UnitTestC/InheritTest.h
@@ -0,0 +1,4 @@
+#include "test_defs.h"
+#include "../COOP/COOP.h"
+#include"../COOP_C_TestProxy/BaseClassExample.h"
+#include"../COOP_C_TestProxy/Rectangle.h"
