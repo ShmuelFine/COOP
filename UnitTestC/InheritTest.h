cfc788eb2614360bfdc7a09807027d4c4978905b
RonenCode <212377808@mby.co.il>
2022-06-01 12:30:44 +0300

    Adding folder again

---
+++ UnitTestC/InheritTest.h
@@ -0,0 +1,7 @@
+#include "test_defs.h"
+#include "../COOP/COOP.h"
+#include"../COOP_C_TestProxy/DerivedClassExample.h"
+
+FUN_DECL(Inheritence_WhenCallingCtorOfDrivedClass_BaseClassCtorIsCalledAsWell);
+FUN_DECL(Inheritence_WhenVirtualFunctionOverriden_ThenCallInvokesOverridingFunc);
+FUN_DECL(Inheritence_WhenCallingNonVirtualFunciton_Sanity);
---
+++ UnitTestC/runTest.h
@@ -0,0 +1,4 @@
+#include "test_defs.h"
+#include "ClassTest.h"
+#include "InheritTest.h"
+#include "rbTreeIterUnitTest.h"
