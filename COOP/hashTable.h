36ff35c7b89f0119f12092fbb48bde21fdc60bff
RonenCode <212377808@mby.co.il>
2022-06-19 16:46:26 +0300

    start to work with Hash table
    Former-commit-id: 19e84c51ac82e8b903050298d072fa84c6309d33

---
+++ COOP/hashTable.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "coop.h"
+//#include <stdio.h>
+//#include <stdlib.h>
+DEF_CLASS(DataItem);
+int key;
+int data;
+END_DEF(DataItem);
+DEF_CLASS(HashTable);
+int key;
+int data;
+END_DEF(HashTable);
