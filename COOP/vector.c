d20c30364d85573334825ef4ed064d64024bbcfd
ShmuelFine <shmuel.v.fine@gmail.com>
2022-12-07 12:44:29 +0200

    tmp obj before reverting...

--- COOP/vector.c
+++ COOP/vector.c
@@ -39,6 +39,7 @@ END_FUN;
 IMPL_AT_OF_TYPE(int);
 IMPL_AT_OF_TYPE(char);
 IMPL_AT_OF_TYPE(float);
+//IMPL_AT_OF_TYPE(object);
 
 #define IMPL_SET_OF_TYPE(type)\
 MEM_FUN_IMPL(GenericVector, set_ ##type, MEM_SIZE_T i, type val) {\
@@ -52,6 +53,7 @@ END_FUN;
 IMPL_SET_OF_TYPE(int);
 IMPL_SET_OF_TYPE(char);
 IMPL_SET_OF_TYPE(float);
+//IMPL_SET_OF_TYPE(object);
 
 #define IMPL_GET_OF_TYPE(type)\
 MEM_FUN_IMPL(GenericVector, get_ ##type, MEM_SIZE_T i, type * val) {\
@@ -116,6 +118,7 @@ MEM_FUN_IMPL(GenericVector, push_back_ ##type, type val) {\
 IMPL_PUSH_OF_TYPE(int);
 IMPL_PUSH_OF_TYPE(char);
 IMPL_PUSH_OF_TYPE(float);
+//IMPL_PUSH_OF_TYPE(object);
 
 
 MEM_FUN_IMPL(GenericVector, __pop_back_generic, char* buff, MEM_SIZE_T buff_size)
@@ -138,6 +141,7 @@ MEM_FUN_IMPL(GenericVector, pop_back_ ##type, type * val) {\
 IMPL_POP_OF_TYPE(int);
 IMPL_POP_OF_TYPE(char);
 IMPL_POP_OF_TYPE(float);
+//IMPL_POP_OF_TYPE(object);
 
 INIT_CLASS(GenericVector)
 BIND(GenericVector, __at_generic);
@@ -175,11 +179,12 @@ MEM_FUN_IMPL(Vector_ ##type, print) {																				\
 	char* format = " %d"; char first_type_name_letter = * #type;													\
 	if (first_type_name_letter == 'c') /*its a char type*/ format = "%c ";											\
 	else if (first_type_name_letter == 'f') /*its a float type*/ format = "%f ";									\
+	/*else if (first_type_name_letter == 'o') its a float type format = "object at %p, ";*/							\
 	type val;																										\
 	for (MEM_SIZE_T i = 0; i < _this->_base.size; i++) 																\
 	{ 																												\
 		MFUN(_this, at), i, & val CALL;																				\
-		printf(format, val); 																						\
+		printf(format, val); 													\
 	} 																												\
 	printf("\n");																									\
 } END_FUN;																											\
@@ -188,7 +193,7 @@ INIT_DERIVED_CLASS(Vector_ ##type, GenericVector);																	\
 BIND(Vector_ ##type, push_back);																					\
 BIND(Vector_ ##type, pop_back);																						\
 BIND(Vector_ ##type, at);																							\
-BIND(Vector_ ##type, print);																							\
+BIND(Vector_ ##type, print);																						\
 END_INIT_CLASS(Vector_ ##type)																						
 
 ////////////////////////////////////////////////
@@ -196,20 +201,23 @@ END_INIT_CLASS(Vector_ ##type)
 IMPL_SPECIFIC_VECTOR_TYPE(int);
 IMPL_SPECIFIC_VECTOR_TYPE(char);
 IMPL_SPECIFIC_VECTOR_TYPE(float);
+//IMPL_SPECIFIC_VECTOR_TYPE(object);
 
 //
-//DEF_DERIVED_CTOR(Vector_float, GenericVector) SUPER, sizeof(float) ME {} END_DERIVED_CTOR							
-//DEF_DERIVED_DTOR(Vector_float, GenericVector) {} END_DERIVED_DTOR													
+//DEF_DERIVED_CTOR(Vector_object, GenericVector) SUPER, sizeof(object) ME {} END_DERIVED_CTOR							
+//DEF_DERIVED_DTOR(Vector_object, GenericVector) {} END_DERIVED_DTOR													
 //																													
-//MEM_FUN_IMPL(Vector_float, push_back, float val) { FUN_BASE(_this, push_back_float), val CALL; } END_FUN;		
-//MEM_FUN_IMPL(Vector_float, pop_back, float * val)	{ FUN_BASE(_this, pop_back_float), val CALL; } END_FUN;		
-//MEM_FUN_IMPL(Vector_float, at, MEM_SIZE_T i, float * val) { FUN_BASE(_this, at_float), i, val CALL; } END_FUN;	
-//MEM_FUN_IMPL(Vector_float, resize, MEM_SIZE_T new_capacity) { FUN_BASE(_this, resize), new_capacity CALL; }END_FUN;
-//MEM_FUN_IMPL(Vector_float, print) {
+//MEM_FUN_IMPL(Vector_object, push_back, object val) { FUN_BASE(_this, push_back_object), val CALL; } END_FUN;		
+//MEM_FUN_IMPL(Vector_object, pop_back, object * val)	{ FUN_BASE(_this, pop_back_object), val CALL; } END_FUN;		
+//MEM_FUN_IMPL(Vector_object, at, MEM_SIZE_T i, object * val) { FUN_BASE(_this, at_object), i, val CALL; } END_FUN;	
+//MEM_FUN_IMPL(Vector_object, get, MEM_SIZE_T i, object* val) { FUN_BASE(_this, get_object), i, val CALL; } END_FUN;	\
+//MEM_FUN_IMPL(Vector_object, set, MEM_SIZE_T i, object val) { FUN_BASE(_this, set_object), i, val CALL; } END_FUN;	\
+//MEM_FUN_IMPL(Vector_object, resize, MEM_SIZE_T new_capacity) { FUN_BASE(_this, resize), new_capacity CALL; }END_FUN;
+//MEM_FUN_IMPL(Vector_object, print) {
 //	char* format = " %d";
 //	if ('f' == 'c') /*its a char type*/ format = "%c ";
 //	if ('f' == 'f') /*its a char type*/ format = "%f ";
-//	float val;
+//	object val;
 //	for (int i = 0; i < _this->_base.size; i++) 
 //	{ 
 //		MFUN(_this, at), i, & val CALL;
@@ -217,8 +225,8 @@ IMPL_SPECIFIC_VECTOR_TYPE(float);
 //	} 
 //} END_FUN;
 //
-//INIT_DERIVED_CLASS(Vector_float, GenericVector);																	
-//BIND(Vector_float, push_back);																					
-//BIND(Vector_float, pop_back);																						
-//BIND(Vector_float, at);																							
-//END_INIT_CLASS(Vector_float)																						
+//INIT_DERIVED_CLASS(Vector_object, GenericVector);																	
+//BIND(Vector_object, push_back);																					
+//BIND(Vector_object, pop_back);																						
+//BIND(Vector_object, at);																							
+//END_INIT_CLASS(Vector_object)																						
