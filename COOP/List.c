9889c25c6b5cdb8b94dc53026e3851f1578ef2e9
Tzila-m <tzmenkes832@gmail.com>
2025-08-31 16:07:08 +0300

    changed in List.h #27

---
+++ COOP/List.c
@@ -0,0 +1,14 @@
+#include "List.h"
+#include "COOP.h"
+#include <stdlib.h>
+#include <string.h>
+
+DEF_CTOR(GenericList, MEM_SIZE_T dataTypeSize)
+{
+	_this->size = 0;
+	_this->capacity = 0;
+	_this->elementSize = dataTypeSize;
+
+	_this->data = NULL;
+}
+END_CTOR
