464e39a6dc7acdc783c533ffa831ee63effe1d1c
michal bakshi <michalb5980@gmail.com>
2025-09-11 01:17:11 +0300

    Improve iterator FOR loop macro for clarity and efficiency#26

--- COOP/Iterator.h
+++ COOP/Iterator.h
@@ -18,54 +18,47 @@ END_DEF(Iterator);
 
 FUNCTIONS(Iterator, IteratorCategory category);
 
-MEM_FUN_DECL(Iterator, equals, object* other, bool* out_equal);
+MEM_FUN_DECL(Iterator, equals, Iterator* other, bool* out_equal);
 MEM_FUN_DECL(Iterator, next);
 MEM_FUN_DECL(Iterator, prev);
 MEM_FUN_DECL(Iterator, get_ref, void** out_ptr);
 MEM_FUN_DECL(Iterator, get_cref, const void** out_ptr);
-MEM_FUN_DECL(Iterator, distance, object* other, ptrdiff_t* out_dist);
+MEM_FUN_DECL(Iterator, distance, Iterator* other, ptrdiff_t* out_dist);
 MEM_FUN_DECL(Iterator, advance, ptrdiff_t n);
 MEM_FUN_DECL(Iterator, reset_begin);
 
 END_FUNCTIONS(Iterator);
 
 
-#define ITER_EQUALS(IT_A, IT_B, OUT_BOOL)         MFUN((IT_A), equals), (object*)(IT_B), (OUT_BOOL) CALL
+#define ITER_EQUALS(IT_A, IT_B, OUT_BOOL)         MFUN((IT_A), equals), (IT_B), (OUT_BOOL) CALL
 #define ITER_NEXT(IT)                             MFUN((IT), next) CALL
 #define ITER_PREV(IT)                             MFUN((IT), prev) CALL
 #define ITER_GET_REF(IT, OUT_VOIDPTR)             MFUN((IT), get_ref), (void**)(OUT_VOIDPTR) CALL
 #define ITER_GET_CREF(IT, OUT_CVOIDPTR)           MFUN((IT), get_cref), (const void**)(OUT_CVOIDPTR) CALL
-#define ITER_DISTANCE(IT_A, IT_B, OUT_DIST)       MFUN((IT_A), distance), (object*)(IT_B), (OUT_DIST) CALL
+#define ITER_DISTANCE(IT_A, IT_B, OUT_DIST)       MFUN((IT_A), distance), (IT_B), (OUT_DIST) CALL
 #define ITER_ADVANCE(IT, N)                       MFUN((IT), advance), (N) CALL
 #define ITER_CATEGORY(IT)   ((IT)->_category)
+#define ITER_RESET_BEGIN(IT)                      MFUN((IT), reset_begin) CALL
 
-#define ITER_CONTINUE do { __iter_skip_next = true; MFUN(_it, next) CALL; goto __ITER_CONTINUE_LABEL__; } while(0)
+#define ITER_CONTINUE do { goto __ITER_CONTINUE_LABEL__;} while(0)
 
+#define ITER_FOR(ElemType, varName, objPtr)          \
+{                                                    \
+       bool __eq = 0;                                \
+       Iterator *_it = &((objPtr)->_base.begin_iter);\
+       Iterator *_end = &((objPtr)->_base.end_iter); \
+       FOR (;!(__eq);) {                             \
+         ITER_EQUALS(_it,_end,&__eq);                \
+         if (__eq){ITER_RESET_BEGIN(_it); break;}    \
+         const ElemType *_p_##varName = NULL;        \
+         ITER_GET_CREF(_it,&_p_##varName);           \
+         ElemType varName = *_p_##varName;           \
 
-#define ITER_FOR(ElemType, varName, objPtr)                              \
-    bool __eq = 0;                      \
-    bool __iter_skip_next = false;                                       \
-    for (Iterator* _it = (Iterator*)&((objPtr)->_base.begin_iter),          \
-                  *_end = (Iterator*)&((objPtr)->_base.end_iter);           \
-         !(__eq)&&!(IS_BREAKING); )                                                              \
-        for (;;) {                                                          \
-            MFUN(_it, equals), (object*)_end, &__eq CALL;                   \
-            if (__eq) break;                                                \
-            const ElemType *_p_##varName = NULL;                            \
-            MFUN(_it, get_cref), &_p_##varName CALL;                        \
-            ElemType varName = *_p_##varName;                               \
-            SCOPE_START;
-
-#define END_ITER_FOR \
-        __ITER_CONTINUE_LABEL__: \
-        _scope_obj_list_call_dtors(&_scope_obj_list);                \
-        if (!IS_BREAKING && !IS_IN_RETURNING() && !IS_IN_THROWING() && !__iter_skip_next) \
-            MFUN(_it, next) CALL;                                    \
-        __iter_skip_next = false;                                    \
-    }                                                                \
-    if(__eq || IS_BREAKING ) MFUN(_it, reset_begin) CALL;\
-    if (IS_BREAKING) { break; IS_BREAKING=false; }                 \
-    else if (IS_IN_RETURNING() || IS_IN_THROWING()) break;           \
+#define END_ITER_FOR                 \
+            __ITER_CONTINUE_LABEL__: \
+            ITER_NEXT(_it);          \
+        END_LOOP                     \
+      }                              \
 }
 
 
