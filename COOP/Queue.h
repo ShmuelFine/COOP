2203313fed570227e3c7f12517ea9c87e61f3dd6
michal bakshi <michalb5980@gmail.com>
2025-09-18 20:41:55 +0300

    Implementing a queue using a list and using an iterator

---
+++ COOP/Queue.h
@@ -0,0 +1,80 @@
+#ifndef __COOP__QUEUE__H_
+#define __COOP__QUEUE__H_
+
+#include "COOP.h"
+#include "Iterator.h"
+#include "SharedObjPtr.h"
+#include "List.h"   
+#include <stdbool.h>
+#include <stddef.h>
+#include "SharedObjPtr.h"
+
+/* =============================
+ *        Generic Queue
+ * ============================= */
+DEF_CLASS(GenericQueue);
+GenericList list;
+END_DEF(GenericQueue);
+
+FUNCTIONS(GenericQueue);
+
+/* basic ops (Vector-style generic + typed wrappers) */
+MEM_FUN_DECL(GenericQueue, enqueue_generic, const char* buff, MEM_SIZE_T buff_size);
+MEM_FUN_DECL(GenericQueue, dequeue_generic, char* out_buff, MEM_SIZE_T buff_size);
+MEM_FUN_DECL(GenericQueue, front_generic, char** out_ptr);
+MEM_FUN_DECL(GenericQueue, front_generic_cref, const char** out_ptr);
+MEM_FUN_DECL(GenericQueue, clear);
+MEM_FUN_DECL(GenericQueue, size, MEM_SIZE_T* out_size);
+MEM_FUN_DECL(GenericQueue, begin, Iterator** out_it);
+MEM_FUN_DECL(GenericQueue, end, Iterator** out_it);
+MEM_FUN_DECL(GenericQueue, empty, bool* out_empty);
+MEM_FUN_DECL(GenericQueue, print);
+
+/* ====== Typed helpers ====== */
+MEM_FUN_DECL(GenericQueue, enqueue_int, int val);
+MEM_FUN_DECL(GenericQueue, enqueue_char, char val);
+MEM_FUN_DECL(GenericQueue, enqueue_float, float val);
+MEM_FUN_DECL(GenericQueue, enqueue_objSPtr, objSPtr val);
+
+MEM_FUN_DECL(GenericQueue, dequeue_int, int* out_val);
+MEM_FUN_DECL(GenericQueue, dequeue_char, char* out_val);
+MEM_FUN_DECL(GenericQueue, dequeue_float, float* out_val);
+MEM_FUN_DECL(GenericQueue, dequeue_objSPtr, objSPtr* out_val);
+
+MEM_FUN_DECL(GenericQueue, front_int, int** out_ptr);
+MEM_FUN_DECL(GenericQueue, front_char, char** out_ptr);
+MEM_FUN_DECL(GenericQueue, front_float, float** out_ptr);
+MEM_FUN_DECL(GenericQueue, front_objSPtr, objSPtr** out_ptr);
+
+MEM_FUN_DECL(GenericQueue, front_int_cref, const int** out_ptr);
+MEM_FUN_DECL(GenericQueue, front_char_cref, const char** out_ptr);
+MEM_FUN_DECL(GenericQueue, front_float_cref, const float** out_ptr);
+MEM_FUN_DECL(GenericQueue, front_objSPtr_cref, const objSPtr** out_ptr);
+
+END_FUNCTIONS(GenericQueue);
+
+/* =============================
+ *     Specific Queue types
+ * ============================= */
+#define DECLARE_SPECIFIC_QUEUE_TYPE(type)                                   \
+DEF_DERIVED_CLASS(Queue_##type, GenericQueue);                              \
+END_DEF_DERIVED(Queue_##type);                                              \
+DERIVED_FUNCTIONS(Queue_##type, GenericQueue);                              \
+MEM_FUN_DECL(Queue_##type, enqueue, type val);                              \
+MEM_FUN_DECL(Queue_##type, dequeue, type* out_val);                         \
+MEM_FUN_DECL(Queue_##type, front, type** out_ptr);                          \
+MEM_FUN_DECL(Queue_##type, front_cref, const type** out_ptr);               \
+MEM_FUN_DECL(Queue_##type, clear);                                          \
+MEM_FUN_DECL(Queue_##type, size, MEM_SIZE_T* out_size);                     \
+MEM_FUN_DECL(Queue_ ##type, begin, Iterator **out_it);                     \
+MEM_FUN_DECL(Queue_ ##type, end, Iterator **out_it);                        \
+MEM_FUN_DECL(Queue_##type, empty, bool* out_empty);                         \
+MEM_FUN_DECL(Queue_##type, print);                                         \
+END_DERIVED_FUNCTIONS(Queue_##type);
+
+DECLARE_SPECIFIC_QUEUE_TYPE(int);
+DECLARE_SPECIFIC_QUEUE_TYPE(char);
+DECLARE_SPECIFIC_QUEUE_TYPE(float);
+DECLARE_SPECIFIC_QUEUE_TYPE(objSPtr);
+
+#endif 
---
+++ UnitTestC/QueueUnitTest.c
@@ -0,0 +1,128 @@
+#include "UnitTestsInfra.h"
+#include "MathUtils.h"
+#include <stdio.h>
+#include "QueueUnitTest.h"
+
+/* ============================
+ *   BASIC QUEUE BEHAVIOR
+ * ============================ */
+TEST_FUN_IMPL(QueueTest, Queue_fifo_order)
+{
+    CREATE(Queue_int, q) CALL;
+
+    MFUN(&q, enqueue), 1 CALL;
+    MFUN(&q, enqueue), 2 CALL;
+    MFUN(&q, enqueue), 3 CALL;
+
+    int val;
+    MFUN(&q, dequeue), & val CALL; ASSERT(1 == val);
+    MFUN(&q, dequeue), & val CALL; ASSERT(2 == val);
+    MFUN(&q, dequeue), & val CALL; ASSERT(3 == val);
+}
+END_FUN
+
+/* ============================
+ *   FRONT / SIZE / EMPTY
+ * ============================ */
+TEST_FUN_IMPL(QueueTest, Queue_front_size_empty)
+{
+    CREATE(Queue_int, q) CALL;
+
+    bool is_empty = false;
+    MFUN(&q, empty), & is_empty CALL;
+    ASSERT(is_empty == 1);
+
+    MFUN(&q, enqueue), 42 CALL;
+
+    MEM_SIZE_T sz = 0;
+    MFUN(&q, size), & sz CALL;
+    ASSERT(1 == (int)sz);
+
+    int* p = NULL;
+    MFUN(&q, front), & p CALL;
+    ASSERT_NOT_NULL(p);
+    ASSERT(42 == *p);
+}
+END_FUN
+
+/* ============================
+ *   CLEAR BEHAVIOR
+ * ============================ */
+TEST_FUN_IMPL(QueueTest, Queue_clear_resets)
+{
+    CREATE(Queue_int, q) CALL;
+
+    FOR(int i = 0; i < 5; ++i) {
+        MFUN(&q, enqueue), i CALL;
+    } END_LOOP;
+
+    MFUN(&q, clear) CALL;
+
+    bool is_empty = true;
+    MFUN(&q, empty), & is_empty CALL;
+    ASSERT(is_empty == 1);
+
+    MEM_SIZE_T sz = 123;
+    MFUN(&q, size), & sz CALL;
+    ASSERT(0 == (int)sz);
+
+    /* iteration after clear should do nothing */
+    int sum = 0;
+    ITER_FOR(int, v, (GenericQueue*)&q) {
+        sum += v;
+    } END_ITER_FOR
+        ASSERT(0 == sum);
+}
+END_FUN
+
+/* ============================
+ *   ITERATOR FOREACH
+ * ============================ */
+TEST_FUN_IMPL(QueueTest, Queue_iterator_foreach)
+{
+    CREATE(Queue_int, q) CALL;
+
+    int data[] = { 10, 20, 30 };
+    FOR(int i = 0; i < 3; ++i) {
+        MFUN(&q, enqueue), data[i] CALL;
+    } END_LOOP;
+
+    int sum = 0;
+    ITER_FOR(int, v, (GenericQueue*)&q) {
+        sum += v;
+    } END_ITER_FOR
+        ASSERT(60 == sum);
+
+    /* test continue: skip 20 */
+    int sum2 = 0;
+    ITER_FOR(int, v, (GenericQueue*)&q) {
+        IF(v == 20)
+        {
+            ITER_CONTINUE;
+        } END_IF
+            sum2 += v;
+    } END_ITER_FOR
+        ASSERT(40 == sum2);
+
+    /* test break: stop at first 30 */
+    int sum3 = 0;
+    ITER_FOR(int, v, (GenericQueue*)&q) {
+        IF(v == 30)
+        {
+            BREAK;
+        } END_IF
+            sum3 += v;
+    } END_ITER_FOR
+        ASSERT(30 == sum3);
+}
+END_FUN
+
+/* ============================
+ *   SUITE
+ * ============================ */
+INIT_TEST_SUITE(QueueTest)
+BIND_TEST(QueueTest, Queue_fifo_order);
+BIND_TEST(QueueTest, Queue_front_size_empty);
+BIND_TEST(QueueTest, Queue_clear_resets);
+BIND_TEST(QueueTest, Queue_iterator_foreach);
+END_INIT_TEST_SUITE(QueueTest)
