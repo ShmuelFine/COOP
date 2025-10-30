0a34e14eb917c270f0e4535d769850e50a3db4f8
reizy <reizy215@gmail.com>
2025-10-30 15:45:22 +0200

    Creating each test case in a function #43

--- Compare/benchmarks/coop/coop_bench_binary_tree.c
+++ Compare/benchmarks/coop/coop_bench_binary_tree.c
@@ -1,84 +1,103 @@
- #include <stdio.h>
- #include <stdlib.h>
- #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
- #include "COOP.h"
- #include "BinaryTree.h"
+#include "COOP.h"
+#include "BinaryTree.h"
 
+/* -------- COOP functions (not static C) -------- */
 
- FUN_IMPL(main, int argc, char** argv)
- {
-     FUN(init_global_memory) 0, HEAP_BASED_MEMORY CALL;
+FUN_IMPL(phase_insert, BTree_int* bt, MEM_SIZE_T N)
+{
+    FOR(MEM_SIZE_T i = 0; i < N; ++i) {
+        MFUN(bt, insert), (int)i CALL;
+    } END_LOOP;
+}
+END_FUN
 
-     const char* phase = (argc > 1) ? argv[1] : "all";
-     MEM_SIZE_T  N = (argc > 2) ? (MEM_SIZE_T)strtoull(argv[2], NULL, 10) : (MEM_SIZE_T)100000;
+FUN_IMPL(phase_remove, BTree_int* bt, MEM_SIZE_T N)
+{
+    /* prepare */
+    FOR(MEM_SIZE_T i = 0; i < N; ++i) {
+        MFUN(bt, insert), (int)i CALL;
+    } END_LOOP;
 
-     CREATE(BTree_int, bt) CALL;
+    /* remove */
+    FOR(MEM_SIZE_T i = 0; i < N; ++i) {
+        bool removed = false;
+        MFUN(bt, remove), (int)i, & removed CALL;
+    } END_LOOP;
+}
+END_FUN
 
-     // ---- insert ----
-     if (strcmp(phase, "insert") == 0)
-     {
-         FOR(MEM_SIZE_T i = 0; i < N; ++i)
-         {
-             MFUN(&bt, insert), (int)i CALL;
-         }
-         END_LOOP;
-     }
-     // ---- remove ----
-     else if (strcmp(phase, "remove") == 0)
-     {
-         FOR(MEM_SIZE_T i = 0; i < N; ++i)
-         {
-             MFUN(&bt, insert), (int)i CALL;
-         }
-         END_LOOP;
+FUN_IMPL(phase_size, BTree_int* bt, MEM_SIZE_T N)
+{
+    MEM_SIZE_T s = 0;
+    FOR(MEM_SIZE_T i = 0; i < N; ++i) {
+        MFUN(bt, get_size), & s CALL;
+    } END_LOOP;
+}
+END_FUN
 
-         FOR(MEM_SIZE_T i = 0; i < N; ++i)
-         {
-             bool removed = false;
-             MFUN(&bt, remove), (int)i, & removed CALL;
-         }
-         END_LOOP;
-     }
-     // ---- size ----
-     else if (strcmp(phase, "size") == 0)
-     {
-         MEM_SIZE_T s = 0;
-         FOR(int i = 0; i < N; i++) {
-             MFUN(&bt, get_size), & s CALL;
-         }END_LOOP;
-     }
-     // ---- empty (API is_empty) ----
-     else if (strcmp(phase, "empty") == 0)
-     {
-         bool is_empty = false;
-         FOR(int i = 0; i < N; i++) {
-             MFUN(&bt, is_empty), & is_empty CALL;
-         }END_LOOP;
-     }
-     // ---- all ----
-     else
-     {
-         MEM_SIZE_T s = 0;
-         bool is_empty = false;
+FUN_IMPL(phase_empty, BTree_int* bt, MEM_SIZE_T N)
+{
+    bool is_empty = false;
+    FOR(MEM_SIZE_T i = 0; i < N; ++i) {
+        MFUN(bt, is_empty), & is_empty CALL;
+    } END_LOOP;
+}
+END_FUN
 
-         FOR(MEM_SIZE_T i = 0; i < N; ++i)
-         {
-             MFUN(&bt, insert), (int)i CALL;
-             MFUN(&bt, get_size), & s CALL;
-             MFUN(&bt, is_empty), & is_empty CALL;
-         }
-         END_LOOP;
+FUN_IMPL(phase_all, BTree_int* bt, MEM_SIZE_T N)
+{
+    MEM_SIZE_T s = 0;
+    bool is_empty = false;
 
-         FOR(MEM_SIZE_T i = 0; i < N; ++i)
-         {
-             bool removed = false;
-             MFUN(&bt, remove), (int)i, & removed CALL;
-         }
-         END_LOOP;
-     }
+    FOR(MEM_SIZE_T i = 0; i < N; ++i) {
+        MFUN(bt, insert), (int)i CALL;
+        MFUN(bt, get_size), & s CALL;
+        MFUN(bt, is_empty), & is_empty CALL;
+    } END_LOOP;
 
-     DESTROY(&bt);
-     RETURN(0);
- }
- END_FUN
+    FOR(MEM_SIZE_T i = 0; i < N; ++i) {
+        bool removed = false;
+        MFUN(bt, remove), (int)i, & removed CALL;
+    } END_LOOP;
+}
+END_FUN
+
+/* ------------------ main ------------------ */
+FUN_IMPL(main, int argc, char** argv)
+{
+    FUN(init_global_memory) 0, HEAP_BASED_MEMORY CALL;
+
+    const char* phase = (argc > 1) ? argv[1] : "all";
+    MEM_SIZE_T  N = (argc > 2) ? (MEM_SIZE_T)strtoull(argv[2], NULL, 10) : (MEM_SIZE_T)10000;
+
+    CREATE(BTree_int, bt) CALL;
+
+    if (strcmp(phase, "insert") == 0)
+    {
+        FUN(phase_insert)& bt, N CALL;
+    }
+    else if (strcmp(phase, "remove") == 0)
+    {
+        FUN(phase_remove)& bt, N CALL;
+    }
+    else if (strcmp(phase, "size") == 0)
+    {
+        FUN(phase_size)& bt, N CALL;
+    }
+    else if (strcmp(phase, "empty") == 0)
+    {
+        FUN(phase_empty)& bt, N CALL;
+    }
+    else
+    {
+        FUN(phase_all)& bt, N CALL;
+    }
+
+    DESTROY(&bt);
+    RETURN(0);
+}
+END_FUN
--- Compare/benchmarks/pure_c/pure_c_bench_binary_tree.c
+++ Compare/benchmarks/pure_c/pure_c_bench_binary_tree.c
@@ -6,51 +6,61 @@
 
 #include "bt_pure.h"
 
+/* ---- phase helpers ---- */
+static void phase_insert(GenericBinaryTree* bt, int N) {
+    for (int i = 0; i < N; ++i) {
+        insert(bt, i);
+    }
+}
+
+static void phase_remove(GenericBinaryTree* bt, int N) {
+    /* prepare */
+    for (int i = 0; i < N; ++i) {
+        insert(bt, i);
+    }
+    /* remove */
+    for (int i = 0; i < N; ++i) {
+        (void)bt_remove(bt, i);
+    }
+}
+
+static void phase_size(GenericBinaryTree* bt, int N) {
+    for (int i = 0; i < N; ++i) {
+        (void)get_size(bt);
+    }
+}
+
+static void phase_empty(GenericBinaryTree* bt, int N) {
+    for (int i = 0; i < N; ++i) {
+        (void)is_empty(bt);
+    }
+}
+
+static void phase_all(GenericBinaryTree* bt, int N) {
+    for (int i = 0; i < N; ++i) {
+        insert(bt, i);
+        (void)get_size(bt);
+        (void)is_empty(bt);
+    }
+    for (int i = 0; i < N; ++i) {
+        (void)bt_remove(bt, i);
+    }
+}
+
+/* ---- main ---- */
 int main(int argc, char** argv)
 {
     const char* phase = (argc > 1) ? argv[1] : "all";
-    int  N = (argc > 2) ? (int)strtoull(argv[2], NULL, 10) : 1000000;
+    int N = (argc > 2) ? (int)strtoull(argv[2], NULL, 10) : 1000000;
 
     GenericBinaryTree bt;
     bt_init(&bt);
 
-    if (strcmp(phase, "insert") == 0) {
-        for (int i = 0; i < N; ++i) {
-            insert(&bt, i);
-        }
-    }
-    else if (strcmp(phase, "remove") == 0) {
-        for (int i = 0; i < N; ++i) {
-            insert(&bt, i);
-        }
-        for (int i = 0; i < N; ++i) {
-            (void)bt_remove(&bt, i);
-        }
-    }
-    else if (strcmp(phase, "size") == 0) {
-        for (int i = 0; i < N; ++i){
-            (void)get_size(&bt);
-        }
-    }
-    else if (strcmp(phase, "empty") == 0) {
-        for (int i = 0; i < N; ++i) {
-            (void)is_empty(&bt);
-        }
-    }
-    else {
-        int size = 0;
-		bool isEmpty = false;
-
-        for (int i = 0; i < N; ++i) {
-            insert(&bt, i);
-            size = get_size(&bt);
-            isEmpty = is_empty(&bt);
-        }
-
-        for (int i = 0; i < N; ++i){
-            (void)bt_remove(&bt, i);
-        }
-    }
+    if (strcmp(phase, "insert") == 0)      phase_insert(&bt, N);
+    else if (strcmp(phase, "remove") == 0) phase_remove(&bt, N);
+    else if (strcmp(phase, "size") == 0)   phase_size(&bt, N);
+    else if (strcmp(phase, "empty") == 0)  phase_empty(&bt, N);
+    else                                   phase_all(&bt, N);
 
     bt_destroy(&bt);
     return 0;
