0ae49f5c6e75063df2daf0cf673e8dd238f721f4
Tzila-m <tzmenkes832@gmail.com>
2025-11-05 10:48:38 +0200

    Division into functions to the bench_list file #45

--- Compare/benchmarks/coop/bench_list_ops.c
+++ Compare/benchmarks/coop/bench_list_ops.c
@@ -4,133 +4,176 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "../COOP.h"
-#include "../List.h"
-#include "../Iterator.h"
+#include "COOP.h"
+#include "List.h"
+
+
+// push_back
+FUN_IMPL(push_back, List_int* lst, int N)
+{
+    FOR(int i = 0; i < N; ++i)
+    {
+        MFUN(lst, push_back), (int)i CALL;
+    }
+    END_LOOP;
+}END_FUN
+
+// push_front
+FUN_IMPL(push_front, List_int* lst, int N)
+{
+    FOR(int i = 0; i < N; ++i)
+    {
+        MFUN(lst, push_front), (int)i CALL;
+    }
+    END_LOOP;
+}END_FUN
+
+// pop_back
+FUN_IMPL(pop_back, List_int* lst, int N)
+{
+    int out = 0;
+    FOR(int i = 0; i < N; ++i)
+    {
+        MFUN(lst, pop_back), & out CALL;
+        (void)out; // avoid warnings
+
+    }
+    END_LOOP;
+}END_FUN
+
+// pop_front 
+FUN_IMPL(pop_front, List_int* lst, int N)
+{
+    int out = 0;
+    FOR(int i = 0; i < N; ++i)
+    {
+        MFUN(lst, pop_front), & out CALL;
+        (void)out; // avoid warnings
+    }
+    END_LOOP;
+}END_FUN
+
+
+// front ??
+FUN_IMPL(front, List_int* lst, int N)
+{
+    MFUN(lst, push_back), 123 CALL;
+
+    int out_val = 0;
+    FOR(int i = 0; i < N; ++i)
+    {
+        MFUN(lst, front), & out_val CALL;
+    }
+    END_LOOP;
+}END_FUN
+
+// back??
+FUN_IMPL(back, List_int* lst, int N)
+{
+
+    int out_val = 0;
+    FOR(int i = 0; i < N; ++i)
+    {
+        MFUN(lst, back), & out_val CALL;
+    }
+    END_LOOP;
+}END_FUN
+
+
+// size
+FUN_IMPL(size, List_int* lst, int N)
+{
+    int size = 0;
+    FOR(int i = 0; i < N; ++i)
+    {
+        MFUN(lst, size), & size CALL;
+    }
+    END_LOOP;
+}END_FUN
+
+// empty
+FUN_IMPL(empty, List_int* lst, int N)
+{
+    bool is_empty = false;
+    FOR(int i = 0; i < N; ++i)
+    {
+        MFUN(lst, empty), & is_empty CALL;
+    }
+    END_LOOP;
+}END_FUN
+
+// clear 
+FUN_IMPL(clear, List_int* lst, int N)
+{
+    MFUN(lst, clear) CALL;
+}END_FUN
+
+// all
+FUN_IMPL(all, List_int* lst, int N)
+{
+    bool is_empty = false;
+    int size = 0;
+    int out_val = 0;
+
+    FOR(int i = 0; i < N / 2; ++i)
+    {
+        MFUN(lst, push_back), (int)i CALL;
+        MFUN(lst, push_front), (int)i CALL;
+        MFUN(lst, front), & out_val CALL;
+        MFUN(lst, back), & out_val CALL;
+        MFUN(lst, size), & size CALL;
+        MFUN(lst, empty), & is_empty CALL;
+    }
+    END_LOOP;
+
+    int out = 0;
+    FOR(int i = 0; i < N / 2; ++i)
+    {
+        MFUN(lst, pop_back), & out CALL;
+        MFUN(lst, pop_front), & out CALL;
+    }END_LOOP;
+
+
+    FOR(int i = 0; i < N; ++i)
+    {
+        MFUN(lst, push_back), (int)i CALL;
+    }END_LOOP;
+    MFUN(lst, clear) CALL;
+}END_FUN
+
+
+// --------------------------- main ---------------------------
 
 FUN_IMPL(main, int argc, char** argv)
 {
     init_global_memory(0, HEAP_BASED_MEMORY);
+
     const char* phase = (argc > 1) ? argv[1] : "all";
-    int  N = (argc > 2) ? (int)strtoull(argv[2], NULL, 10) : (int)1000000;
+    size_t N_size_t = (argc > 2) ? (size_t)strtoull(argv[2], NULL, 10) : (size_t)1000000;
+    int N = (int)N_size_t;
 
     CREATE(List_int, lst) CALL;
 
- 
-
-    IF(strcmp(phase, "pop_back") == 0 ||
-        strcmp(phase, "pop_front") == 0)
-    {
-        FOR(int i = 0; i < N; ++i)
-        {
-            MFUN(&lst, push_back), (int)i CALL;
-        }END_LOOP;
-
-        IF(strcmp(phase, "pop_back") == 0)
-        {
-            FOR(int i = 0; i < N; ++i)
-            {
-                int out = 0;
-                MFUN(&lst, pop_back), & out CALL;
-                (void)out;
-            }END_LOOP;
-        }
-
-        ELSE_IF(strcmp(phase, "pop_front") == 0)
-        {
-            FOR(int i = 0; i < N; ++i)
-            {
-                int out = 0;
-                MFUN(&lst, pop_front), & out CALL;
-                (void)out;
-            }
-            END_LOOP;
-        }END_IF
-    }
-
-    // ---- push_back ----
-    ELSE_IF(strcmp(phase, "push_back") == 0)
-    {
-        FOR(int i = 0; i < N; ++i)
-        {
-            MFUN(&lst, push_back), (const int)i CALL;
-        }
-        END_LOOP;
-    }
-    // ---- push_front ----
+    IF(strcmp(phase, "push_back") == 0)
+        push_back(&lst, N);
     ELSE_IF(strcmp(phase, "push_front") == 0)
-    {
-        FOR(int i = 0; i < N; ++i)
-        {
-            MFUN(&lst, push_front), (const int)i CALL;
-        }
-        END_LOOP;
-    }    
-    // ---- size ----
-    ELSE_IF(strcmp(phase, "size") == 0)
-    {
-        MEM_SIZE_T size = 0;
-        FOR(int i = 0; i < N; ++i)
-        {
-            MFUN(&lst, size), &size CALL;
-        }
-        END_LOOP;
-    }
-    // ---- empty (API size==0) ----
-    ELSE_IF(strcmp(phase, "empty") == 0)
-    {
-        bool is_empty = 0;
-        FOR(int i = 0; i < N; ++i)
-        {
-            MFUN(&lst, empty), &is_empty CALL;
-        }
-        END_LOOP;
-    }
-    ELSE_IF(strcmp(phase, "clear") == 0) {
-        FOR(int i = 0; i < N; ++i)
-        {
-            MFUN(&lst, push_back), (const int)i CALL;
-        }
-        END_LOOP;
-        MFUN(&lst, clear) CALL;
-    }
+        push_front(&lst, N);
+    ELSE_IF(strcmp(phase, "pop_back") == 0)
+        pop_back(&lst, N);
+    ELSE_IF(strcmp(phase, "pop_front") == 0)
+        pop_front(&lst, N);
     ELSE_IF(strcmp(phase, "front") == 0)
-    {
-
-        int pFront = 0;
-        FOR(int i = 0; i < N; ++i)
-        {
-            MFUN(&lst, front), & pFront CALL;
-        }
-        END_LOOP;
-
-    }
+        front(&lst, N);
+    ELSE_IF(strcmp(phase, "back") == 0)
+        back(&lst, N);
+    ELSE_IF(strcmp(phase, "size") == 0)
+        size(&lst, N);
+    ELSE_IF(strcmp(phase, "empty") == 0)
+        empty(&lst, N);
+    ELSE_IF(strcmp(phase, "clear") == 0)
+        clear(&lst, N);
     ELSE
-    {
-        FOR(int i = 0; i < N / 2; ++i)
-        {
-            MEM_SIZE_T size = 0;
-            bool is_empty = 0;
-            MFUN(&lst, push_back), (int)i CALL;
-            MFUN(&lst, push_front), (int)i CALL;
-            MFUN(&lst, size),& size CALL;
-            MFUN(&lst, empty),& is_empty CALL;
-        }
-        END_LOOP;
+        all(&lst, N);
+    END_IF;
 
-        FOR(int i = 0; i < N / 2; ++i)
-        {
-            int out = 0;
-            MFUN(&lst, pop_back),& out CALL;
-            MFUN(&lst, pop_front),& out CALL;
-        }END_LOOP;
-
-        FOR(int i = 0; i < N; ++i)
-        {
-            MFUN(&lst, push_back), (int)i CALL;
-        }END_LOOP;
-        MFUN(&lst, clear) CALL;
-
-    }END_IF
 }END_FUN
-
